0bcd9ebe8e4516c6bb84cec67c8052151ddc26ab
newLoki <newloki91@googlemail.com>
2012-08-31 08:54:21 +0200

    add basic user controller action to get an user by id (and test it)

--- src/Bibi/Controller/UserControllerProvider.php
+++ src/Bibi/Controller/UserControllerProvider.php
@@ -13,8 +13,29 @@ class UserControllerProvider implements \Silex\ControllerProviderInterface
 
         $app->get('/users/{id}', function($id) use ($app)
         {
+
+            /** @var $em \Doctrine\ORM\EntityManager */
+            $em = $app['db.orm.em'];
+            /** @todo fetch not all users */
+            $query = $em->createQuery(
+                'SELECT u.surname, u.lastname, u.email, u.birthdate, u.name
+                    FROM Bibi\Entity\User u
+                    WHERE u.name = ?1');
+            $query->setParameter('1', $id);
+            $result = $query->getSingleResult();
+
             $data = new \stdClass();
             $data->users = array();
+
+            if(!empty($result)) {
+                $user = new \stdClass();
+                foreach($result as $key => $value) {
+                    $user->{$key} = $value;
+                }
+
+                $data->users[] = $user;
+            }
+
             return $app->json($data); //@todo return a specific user, or all if no id given
         })->bind('user.id');
 
--- tests/Tests/Application/UsersControllerTest.php
+++ tests/Tests/Application/UsersControllerTest.php
@@ -7,21 +7,28 @@ use Symfony\Component\HttpFoundation\Session\Storage\MockFileSessionStorage;
  * @todo this is more like an integration test, maybee it is sensefull to move it into a new
  * directoy/test scenario who is called integration test
  */
-class UsersTest extends \Tests\ApplicationTestCase
+class UsersControllerTest extends \Tests\ApplicationTestCase
 {
-    public function testUsersIndexResponse()
+    public function testExistingUserResponse()
     {
         $client = $this->createClient();
-        $crawler = $client->request('GET', '/users/1', array(), array(), array());
+        $crawler = $client->request('GET', '/users/jon', array(), array(), array());
 
         $response = $client->getResponse();
 
         $this->assertEquals(200, $response->getStatusCode());
 
         $data = json_decode($response->getContent());
-        $expectedData = new \stdClass();
-        $expectedData->users = array();
 
+        $user = new \stdClass();
+        $user->name = 'jon';
+        $user->surname = 'john';
+        $user->lastname = 'doe';
+        $user->email = 'john.doe@example.com';
+        $user->birthdate = "1990-01-01";
+
+        $expectedData = new \stdClass();
+        $expectedData->users = array($user);
         $this->assertEquals($expectedData, $data);
     }
 }
