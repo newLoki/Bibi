3929d57e94016175f80babd58497b7fd344fe266
Alberto Assmann <newloki91@googlemail.com>
2012-09-02 19:31:14 +0200

    remove auto id from base entity, makes no sense in my case

--- src/Bibi/Entity/Base.php
+++ src/Bibi/Entity/Base.php
@@ -2,92 +2,19 @@
 
 namespace Bibi\Entity;
 
-/**
-* @HasLifecycleCallbacks
-* @MappedSuperclass
-*/
 abstract class Base
 {
     const DATE_FORMAT = 'Y-m-d\TH:i:s';
 
-    /**
-     * @Id
-     * @Column(type="integer")
-     * @GeneratedValue(strategy="AUTO")
-     * @var int
-     **/
-    protected $id;
-
-    /**
-     * @Column(type="datetime")
-     * @var \DateTime
-     */
-    protected $created;
-
-    /**
-     * @Column(type="datetime")
-     * @var \DateTime
-     */
-    protected $updated;
-
-    /**
-     * Set created date to now, if a new object is constructed
-     * (this is not called on hydration)
-     *
-     * @return void
-     */
-    public function __construct()
+    public function getSimpleObject()
     {
-        $this->created = new \DateTime();
-        $this->updated = $this->created;
-    }
+        $result = new \stdClass();
 
-    /**
-     * Set updated field to now()
-     * @PrePersist
-     */
-    public function createUpdateDateTime()
-    {
-        $this->updated = new \DateTime();
-    }
+        foreach (get_class_vars(get_class($this)) as $key => $value) {
+            $methodName = "get" . ucfirst($key);
+            $result->{$key} = call_user_func(array($this, $methodName));
+        }
 
-    /**
-     * Return, when entity was created
-     *
-     * @return \DateTime
-     */
-    public function getCreated()
-    {
-        return $this->created;
-    }
-
-    /**
-     * Return when entity was updated last time
-     *
-     * @return \DateTime
-     */
-    public function getUpdated()
-    {
-        return $this->updated;
-    }
-
-    /**
-     * Return unique identifier for entity
-     *
-     * @return int
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * Set id for this entity (commonly for testing reasons)
-     *
-     * @param int $id
-     */
-    public function setId($id)
-    {
-        $this->id = (int) $id;
+        return $result;
     }
 }
